c44331f901df6f65b4065096614e4194b573ccf3
Eduardo Arantes Figueiredo <101658322+Duddss@users.noreply.github.com>
2024-11-29 11:18:10 -0300

    Add files via upload

--- site_5/js/custom.js
+++ site_5/js/custom.js
@@ -1,7 +1,7 @@
-(function() {
+(function () {
 	'use strict';
 
-	var tinyslider = function() {
+	var tinyslider = function () {
 		var el = document.querySelectorAll('.testimonial-slider');
 
 		if (el.length > 0) {
@@ -23,51 +23,285 @@
 	};
 	tinyslider();
 
-	
 
 
-	var sitePlusMinus = function() {
+
+	var sitePlusMinus = function () {
 
 		var value,
-    		quantity = document.getElementsByClassName('quantity-container');
+			quantity = document.getElementsByClassName('quantity-container');
 
 		function createBindings(quantityContainer) {
-	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
-	      var increase = quantityContainer.getElementsByClassName('increase')[0];
-	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
-	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
-	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
-	    }
+			var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+			var increase = quantityContainer.getElementsByClassName('increase')[0];
+			var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+			increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
+			decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
+		}
 
-	    function init() {
-	        for (var i = 0; i < quantity.length; i++ ) {
-						createBindings(quantity[i]);
-	        }
-	    };
+		function init() {
+			for (var i = 0; i < quantity.length; i++) {
+				createBindings(quantity[i]);
+			}
+		};
 
-	    function increaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
+		function increaseValue(event, quantityAmount) {
+			value = parseInt(quantityAmount.value, 10);
 
-	        console.log(quantityAmount, quantityAmount.value);
+			console.log(quantityAmount, quantityAmount.value);
 
-	        value = isNaN(value) ? 0 : value;
-	        value++;
-	        quantityAmount.value = value;
-	    }
+			value = isNaN(value) ? 0 : value;
+			value++;
+			quantityAmount.value = value;
+		}
 
-	    function decreaseValue(event, quantityAmount) {
-	        value = parseInt(quantityAmount.value, 10);
+		function decreaseValue(event, quantityAmount) {
+			value = parseInt(quantityAmount.value, 10);
 
-	        value = isNaN(value) ? 0 : value;
-	        if (value > 0) value--;
+			value = isNaN(value) ? 0 : value;
+			if (value > 0) value--;
 
-	        quantityAmount.value = value;
-	    }
-	    
-	    init();
-		
+			quantityAmount.value = value;
+		}
+
+		init();
+
 	};
 	sitePlusMinus();
 
 
 })()
+
+
+function cupom(element) {
+	var type = element.getAttribute('data-gtm-type');
+	var clickType = element.getAttribute('data-gtm-clicktype');
+	var name = element.getAttribute('data-gtm-name');
+	var subName = element.getAttribute('data-gtm-subname');
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "apply_cupom",
+		ev_action: type + ':' + clickType + ':' + name,
+		ev_label: subName
+	});
+}
+
+function addtoCart(element) {
+	var subName = element.getAttribute('data-gtm-subname');
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "add_to_cart",
+		ecommerce: {
+			currency: "BRL",
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: subName,
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	});
+}
+
+function removefromCart() {
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "remove_from_cart",
+		ecommerce: {
+			currency: "BRL",
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: "Nordic Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	});
+}
+
+function viewitemList() {
+	window.dataLayer = window.dataLayer || [];
+	window.dataLayer.push({
+		event: 'view_item_list',
+		ecommerce: {
+			currency: "BRL",
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: "Nordic Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	})
+}
+
+function purchase() {
+	window.dataLayer = window.dataLayer || [];
+	window.dataLayer.push({
+		event: "purchase",
+		ecommerce: {
+			currency: "BRL",
+			cupom: "EDUARDO20",
+			discount: 20,
+			payment_method: "Credit Card",
+			transaction_id: "4719824",
+			revenue: 350.00,
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: "Nordic Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				},
+
+				{
+					item_id: "SKU_54321",
+					item_name: "Kruzo Aero Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	});
+}
+
+function begincheckout() {
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "begin_checkout",
+		ecommerce: {
+			currency: "BRL",
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: "Nordic Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				},
+
+				{
+					item_id: "SKU_54321",
+					item_name: "Kruzo Aero Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	});
+}
+
+function viewCart() {
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "view_cart",
+		ecommerce: {
+			currency: "BRL",
+			items: [
+				{
+					item_id: "SKU_12345",
+					item_name: "Nordic Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				},
+
+				{
+					item_id: "SKU_54321",
+					item_name: "Kruzo Aero Chair",
+					item_brand: "Furni",
+					item_category: "Furniture",
+					item_variant: "White",
+					price: 49.00,
+					quantity: 1
+				}
+			]
+		}
+	});
+}
+
+function select_content(element) {
+	var type = element.getAttribute('data-gtm-type');
+	var clickType = element.getAttribute('data-gtm-clicktype');
+	var name = element.getAttribute('data-gtm-name');
+	var subName = element.getAttribute('data-gtm-subname');
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "select_content",
+		ev_action: type + ':' + clickType + ':' + name,
+		ev_label: subName
+	});
+}
+
+// Adiciona o evento de clique para cada botão, chamando select_content sem criar listeners duplicados
+const buttons = document.querySelectorAll('.post-thumbnail, .persons, .btn-select-content');
+buttons.forEach(button => {
+	button.addEventListener('click', () => {
+		select_content(button);
+	});
+});
+
+
+
+function newsletter_generate(element) {
+	var type = element.getAttribute('data-gtm-type');
+	var clickType = element.getAttribute('data-gtm-clicktype');
+	var name = element.getAttribute('data-gtm-name');
+	var subName = element.getAttribute('data-gtm-subname');
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "newsletter_generate",
+		ev_action: type + ':' + clickType + ':' + name,
+		ev_label: subName
+	});
+}
+
+const buttonsPrimary = document.querySelectorAll('.btn-primary');
+buttonsPrimary.forEach(button => {
+	button.addEventListener('click', () => {
+		newsletter_generate(button);
+	});
+});
+
+function lead(element) {
+	var type = element.getAttribute('data-gtm-type');
+	var clickType = element.getAttribute('data-gtm-clicktype');
+	var name = element.getAttribute('data-gtm-name');
+	var subName = element.getAttribute('data-gtm-subname');
+	window.dataLayer = window.dataLayer || [];
+	dataLayer.push({
+		event: "generate_lead",
+		ev_action: type + ':' + clickType + ':' + name,
+		ev_label: subName,
+		lead_id: "1"
+	});
+}
+
